b4299159a418152192192607f9dd570f74fa3dc2
cirio <cirio89@yahoo.it>
2013-05-07 00:07:58 +0200

    try cath web api method

--- InvoicePrj/Controllers/InvoiceController.cs
+++ InvoicePrj/Controllers/InvoiceController.cs
@@ -15,39 +15,100 @@ namespace InvoicePrj.Controllers
         #region Get
         public IEnumerable<Invoice> GetAllInvoices()
         {
-            return repository.GetAll();
+            try
+            {
+                return repository.GetAll();
+            }
+            catch (Exception ex)
+            {
+
+                throw new HttpResponseException(Request.CreateResponse(HttpStatusCode.InternalServerError, ex.Message)); 
+            }
+            
         }
         public Invoice GetInvoice(int id)
         {
-            Invoice item = repository.Get(id);
-            if (item == null)
-                throw new HttpResponseException(HttpStatusCode.NotFound);
-            return item;
+            try
+            {
+                Invoice item = repository.Get(id);
+                if (item == null)
+                    throw new HttpResponseException(HttpStatusCode.NotFound);
+                return item;
+            }
+            catch (Exception ex)
+            {
+
+                throw new HttpResponseException(Request.CreateResponse(HttpStatusCode.InternalServerError, ex.Message)); 
+            }
+            
         }
         public int GetMaxInvoice()
         {
-            int maxInvoice = repository.GetMax();
-            return maxInvoice +1;
+            try
+            {
+                return Convert.ToInt32(repository.GetMax() + 1);
+            }
+            catch (Exception ex)
+            {
+                throw new HttpResponseException(Request.CreateResponse(HttpStatusCode.InternalServerError, ex.Message));
+            }
+            
         }
         public double GetGAmountTot()
         {
-            return repository.GetGAmountTot();
+            try
+            {
+                return repository.GetGAmountTot();
+            }
+            catch (Exception ex)
+            {
+
+                throw new HttpResponseException(Request.CreateResponse(HttpStatusCode.InternalServerError, ex.Message));
+            }
+            
         }
         public double GetNAmountTot()
         {
-            return repository.GetNAmountTot();
+            try
+            {
+                return repository.GetNAmountTot();
+            }
+            catch (Exception ex)
+            {
+
+                throw new HttpResponseException(Request.CreateResponse(HttpStatusCode.InternalServerError, ex.Message));
+            }
+            
         }
         public double GetTaxTot()
         {
-            return repository.GetTaxTot();
+            try
+            {
+                return repository.GetTaxTot();
+            }
+            catch (Exception ex)
+            {
+
+                throw new HttpResponseException(Request.CreateResponse(HttpStatusCode.InternalServerError, ex.Message));
+            }
+            
         }
         #endregion
         #region Delete
         [System.Web.Http.AcceptVerbs("DELETE","GET")]
         public HttpResponseMessage DeleteInvoice(int id)
         {
-            repository.Remove(id);
-            return new HttpResponseMessage(HttpStatusCode.NoContent);
+            try
+            {
+                repository.Remove(id);
+                return new HttpResponseMessage(HttpStatusCode.OK);
+            }
+            catch (Exception ex)
+            {
+
+                throw new HttpResponseException(Request.CreateResponse(HttpStatusCode.InternalServerError, ex.Message));
+            }
+            
         }
         #endregion
         #region Insert
